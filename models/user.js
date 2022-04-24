5833ff2f0c372f19fa72f9f764d4911d8c1e42d1
McKenna <Mckennaburch19@gmail.com>
2022-04-24 14:52:19 -0500

    updated user models

--- models/user.js
+++ models/user.js
@@ -1,51 +1,31 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-    Username: {
+    username: {
         type: String,
-        Unique:true ,
-        required:true,
-         trim:true
+        unique: true,
+        required: true,
+        trim: true
     },
-
-    Email: {
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
         match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, `Please fill valid email address`],
-        validate: {
-            validator: function() {
-              return new Promise((res, rej) =>{
-                User.findOne({email: this.email, _id: {$ne: this._id}})
-                    .then(data => {
-                        if(data) {
-                            res(false)
-                        } else {
-                            res(true)
-                        }
-                    })
-                    .catch(err => {
-                        res(false)
-                    })
-              })
-            }, message: 'Email Already Taken'
-          }
-        },
-
-     thoughts: [
+    },
+    thoughts: [
         {
             type: Schema.Types.ObjectId,
-              ref: 'Thought'
-         },
-        
-         friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ]
-    },
-
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
     {
         toJSON: {
             virtuals: true
