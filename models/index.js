a990e1645eb613e9ac285e5948229abbb89e5196
McKenna <Mckennaburch19@gmail.com>
2022-04-24 15:04:18 -0500

    added thoughts model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const thoughts = require('./thoughts');
 const user = require('./user');
 
+module.exports = {thoughts,user}
--- models/user.js
+++ models/user.js
@@ -34,5 +34,9 @@ const UserSchema = new Schema({
     }
 );
 
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+}); 
+
 const User = model('User', UserSchema);
 module.exports = User;
