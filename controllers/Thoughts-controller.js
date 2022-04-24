a88d7b765430294c5c7d60adb5b4c686d64c6e01
McKenna <Mckennaburch19@gmail.com>
2022-04-24 14:37:12 -0500

    added bases to start coding sections

---
+++ controllers/Thoughts-controller.js
@@ -0,0 +1,16 @@
+const { User, Thoughts} = require('../models');
+
+//enter code to get ALL thoughts
+
+
+//enter code to get thoughts by ID
+
+//enter code to  create a thought 
+
+//enter code to update thought by ID
+
+//enter code to  delete thought by ID 
+
+//enter code to create a reaction 
+
+
---
+++ controllers/User-controller.js
@@ -0,0 +1,18 @@
+const { User, Thought} = require('../models');
+
+// Get all users 
+
+// get user by ID. 
+
+// create a user 
+
+// update user by ID. 
+
+// Delete user 
+
+
+  //add a friend
+
+  //delete a friend.
+
+  
